c088765dc6d44bb049c8cfd1dcc040687e09182a
FadiBenamar <fadi.benamar@univ-tlse3.fr>
2023-11-04 14:09:34 +0100

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,56 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+
+	public Humain(String nom, String boisson, int argent) {
+		super();
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getargent() {
+		return argent;
+	}
+
+	public String prendreParole() {
+		return "( " + nom + " ) - ";
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + texte);
+	}
+
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boisson + ".");
+	}
+
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+
+	public void acheter(String bien, int prix) {
+		if (argent > prix) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + prix + " sous");
+		}
+	}
+
+}
